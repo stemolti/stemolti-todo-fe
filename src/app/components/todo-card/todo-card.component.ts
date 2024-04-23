c04fc0def75a7f87cdc41cbae6678ffa542661e4
Stefano Agbodan <mystefano95@gmail.com>
2024-04-23 23:46:46 +0200

    Added todo card component

---
+++ src/app/components/todo-card/todo-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-card',
+  templateUrl: './todo-card.component.html',
+  styleUrl: './todo-card.component.css'
+})
+export class TodoCardComponent {
+
+}
